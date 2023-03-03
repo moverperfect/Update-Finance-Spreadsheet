9dec1e94de8cbd46e126363cc367b89adefb05f4
moverperfect <moverperfect@gmail.com>
2023-03-03 22:41:27 +0000

    :rotating_light: Fix isort linter issues

--- scrapers/hargreaves.py
+++ scrapers/hargreaves.py
@@ -1,9 +1,10 @@
 import logging
 import time
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from selenium.webdriver.common.by import By
+
 from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class Hargreaves:
--- scrapers/nutmeg.py
+++ scrapers/nutmeg.py
@@ -1,8 +1,9 @@
 import logging
 import time
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
+
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 
 class Nutmeg:
