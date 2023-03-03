13978104b9b54454eb0c0fef76d66dcad4c6afa0
Jack Moorhouse <moverperfect@gmail.com>
2023-03-03 22:01:57 +0000

    :rotating_light: Fix pylint issue

--- scrapers/nutmeg.py
+++ scrapers/nutmeg.py
@@ -1,4 +1,5 @@
-import logging, time
+import logging
+import time
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
