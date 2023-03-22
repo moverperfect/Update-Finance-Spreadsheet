1a62d3c960ee6ad1787048d410fc0b2921366c00
moverperfect <moverperfect@gmail.com>
2023-03-22 22:33:34 +0000

    ✨ Create the shell for moverperfect middleware

---
+++ middleware/moverperfect.py
@@ -0,0 +1,18 @@
+class Moverperfect:
+    def InsertAll(nutmeg_data, shareworks_data, standard_life_data, hargreaves_data):
+        Moverperfect.__Nutmeg(nutmeg_data)
+        Moverperfect.__Shareworks(shareworks_data)
+        Moverperfect.__Standard_Life(standard_life_data)
+        Moverperfect.__Hargreaves(hargreaves_data)
+
+    def __Nutmeg(nutmeg_data):
+        return
+
+    def __Shareworks(shareworks_data):
+        return
+
+    def __Standard_Life(standard_life_data):
+        return
+
+    def __Hargreaves(hargreaves_data):
+        return
