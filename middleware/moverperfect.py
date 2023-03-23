820d31d09660945ef45ed9a67a995f9d09c4417e
moverperfect <moverperfect@gmail.com>
2023-03-23 12:06:31 +0000

    ✨ Insert net contributions and currentvalue

--- middleware/moverperfect.py
+++ middleware/moverperfect.py
@@ -34,9 +34,11 @@ class Moverperfect:
         """
 
         # Function to find the index of the first empty row within a given range
-        def find_empty_row(sheet, start_row, end_row):
+        def find_empty_row(sheet, worksheet, start_row, end_row, column):
             # Read the specified range of rows from the sheet
-            dates = sheet.read_range("Nutmeg", f"A{start_row}:A{end_row}")
+            dates = sheet.read_range(
+                worksheet, f"{column}{start_row}:{column}{end_row}"
+            )
 
             # Iterate through the rows and find the first empty one
             for index, date in enumerate(dates):
@@ -57,13 +59,13 @@ class Moverperfect:
         end_row = 100
 
         # Find the index of the first empty row within the initial range
-        empty_row_index = find_empty_row(sheet, start_row, end_row)
+        empty_row_index = find_empty_row(sheet, "Nutmeg", start_row, end_row, "A")
 
         # Keep searching for an empty row in the next 100-row blocks until one is found
         while empty_row_index is None:
             start_row += 100
             end_row += 100
-            empty_row_index = find_empty_row(sheet, start_row, end_row)
+            empty_row_index = find_empty_row(sheet, "Nutmeg", start_row, end_row, "A")
 
         # Iterate through the transactions in reversed(ascending) order
         for i, transaction in enumerate(reversed(nutmeg_data["transactions"])):
@@ -94,8 +96,31 @@ class Moverperfect:
                     )
                     # Move to the next row
                     empty_row_index += 1
-                    print("insert data")
                 break
+
+        # Initialize the starting and ending row indices
+        start_row = 1
+        end_row = 100
+
+        # Find the index of the first empty row within the initial range
+        empty_row_index = find_empty_row(
+            sheet, "Nutmeg Monthly", start_row, end_row, "H"
+        )
+
+        # Keep searching for an empty row in the next 100-row blocks until one is found
+        while empty_row_index is None:
+            start_row += 100
+            end_row += 100
+            empty_row_index = find_empty_row(
+                sheet, "Nutmeg Monthly", start_row, end_row, "H"
+            )
+
+        # Write latest data
+        sheet.write_range(
+            "Nutmeg Monthly",
+            f"H{empty_row_index}:I{empty_row_index}",
+            [nutmeg_data["netContributions"], nutmeg_data["currentValue"]],
+        )
 
     @staticmethod
     def __shareworks(shareworks_data):
