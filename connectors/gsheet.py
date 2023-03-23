2f9662fb7504c017d4497dd1845603e7e98117e5
Jack Moorhouse <moverperfect@gmail.com>
2023-03-23 00:50:22 +0000

    ✨ Add nutmeg insertion into transaction worksheet (#36)
    * ✨ Add nutmeg insertion into tansaction worksheet
    
    * 🚨 Fix lint errors

--- connectors/gsheet.py
+++ connectors/gsheet.py
@@ -38,23 +38,42 @@ class GoogleSheets:
     def read_range(self, worksheet_name, range_address):
         """Read the values of a range of cells in a worksheet"""
         # Open the sheet using its key
-        with self.client.open_by_key(self.spreadsheet_id) as sheet:
-            # Get the worksheet by name
-            worksheet = sheet.worksheet(worksheet_name)
-            # Get the range of cells
-            cells = worksheet.range(range_address)
-            # Return the values of the cells as a list
-            return [cell.value for cell in cells]
+        sheet = self.client.open_by_key(self.spreadsheet_id)
+
+        # Get the worksheet by name
+        worksheet = sheet.worksheet(worksheet_name)
+
+        # Get the range of cells
+        cells = worksheet.range(range_address)
+
+        # Return the values of the cells as a list
+        return cells
 
     def write_range(self, worksheet_name, range_address, values):
         """Write values to a range of cells in a worksheet"""
         # Open the sheet using its key
-        with self.client.open_by_key(self.spreadsheet_id) as sheet:
-            # Get the worksheet by name
-            worksheet = sheet.worksheet(worksheet_name)
-            # Get the range of cells
-            cells = worksheet.range(range_address)
-            # Update the values of the cells
-            for cell, value in zip(cells, values):
-                cell.value = value
-            worksheet.update_cells(cells)
+        sheet = self.client.open_by_key(self.spreadsheet_id)
+
+        # Get the worksheet by name
+        worksheet = sheet.worksheet(worksheet_name)
+
+        # Calculate dimensions of the range
+        start_cell, end_cell = range_address.split(":")
+        start_row, start_col = gspread.utils.a1_to_rowcol(start_cell)
+        end_row, end_col = gspread.utils.a1_to_rowcol(end_cell)
+
+        num_rows = end_row - start_row + 1
+        num_cols = end_col - start_col + 1
+
+        # Create a 2D list from the input values
+        values_2d = [values[i : i + num_cols] for i in range(0, len(values), num_cols)]
+
+        # Verify if the dimensions match
+        if len(values_2d) != num_rows or len(values_2d[0]) != num_cols:
+            raise ValueError(
+                "The dimensions of the input values do not match "
+                + "the dimensions of the range"
+            )
+
+        # Update the values of the cells
+        worksheet.update(range_address, values_2d, value_input_option="USER_ENTERED")
